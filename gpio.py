a5382207921a673e83c6b28d307dfabbb6b4bce9
Zeke Pendexter <zpendexter@gmail.com>
2017-04-26 18:08:53 +0000

    Cleanup

--- gpio.py
+++ gpio.py
@@ -23,28 +23,3 @@ class PiControl:
 		return GPIO.input(self.photo_pin)
 
 
-
-class Ledarray:
-
-	LED_Array = [2,3,4,5,6,7,8,9,10,11]
-	GPIO.setmode(GPIO.BCM)
-
-	for led in LED_Array:
-		GPIO.setup(led, GPIO.OUT)
-		GPIO.output(led, GPIO.LOW)
-	
-	def FlickerLightsUp(self):
-		for led in self.LED_Array:
-			GPIO.output(led, GPIO.HIGH)
-			time.sleep(.01)
-#			print("Lighting LED {0}".format(led))
-			GPIO.output(led, GPIO.LOW)
-
-
-	def FlickerLightsDown(self):
-		for led in self.LED_Array:
-			led_num = 13 - led
-			GPIO.output(led_num, GPIO.HIGH)
-#			print("Lighting LED {0}".format(led_num))
-			time.sleep(.01)
-			GPIO.output(led_num, GPIO.LOW)
