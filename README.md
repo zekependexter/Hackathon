e76a8b8e23512d201290ab5bbefd3a1f4c486c9e
Zeke Pendexter <zpendexter@gmail.com>
2017-04-26 16:32:43 +0000

    More stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hackathon
+
+Button must be on pin GPIO5
+Photo thing must be on pin GPIO6
---
+++ room.py
@@ -0,0 +1,13 @@
+
+
+class MeetingRoom:
+
+	Name = "Shift"
+	IsOccupied = 0
+	IsBooked = 0
+	BookerName = "Alex"
+	PersonInRoom = ""
+	WebExInfo = ""
+	WebExPhone = "12072400694"
+
+
