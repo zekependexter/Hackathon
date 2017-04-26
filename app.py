fbe95f545600882ab8e9dd8ce499627f03b9eb39
Zeke Pendexter <zpendexter@gmail.com>
2017-04-26 16:28:42 +0000

    Lunchtime commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from gpio import PiControl
+from room import MeetingRoom
+import time
 
 if __name__ == '__main__':
 
@@ -6,5 +8,24 @@ if __name__ == '__main__':
 		
 	print("running")
 	pi = PiControl()
+
+	room = MeetingRoom()
 	
+	print("Initializing GPIO pins...")	
 	pi.InitButton()
+	pi.InitPhoto()
+	print("Initialization done!")
+	print("We are in: {0}".format(room.Name))
+
+	while(1):
+		if(pi.ButtonStatus() == 0):
+			print("Button Pressed")
+			while(pi.ButtonStatus() == 0):
+				time.sleep(.1)	
+			print("Button Depressed")
+
+		if(pi.PhotoStatus() == 1):
+			print("Photo Triggered")
+			while(pi.PhotoStatus() == 1):
+				time.sleep(.1)	
+			print("Photo Un-Triggered")
--- gpio.py
+++ gpio.py
@@ -4,12 +4,25 @@ import time
 class PiControl:
 
 	button_pin = 5
+	photo_pin = 6
 
 	def InitButton(self):
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.button_pin, GPIO.IN)
 		print("Pin {0} is currently: {1}".format(self.button_pin,GPIO.input(self.button_pin)))
 	
+	def ButtonStatus(self):
+		return GPIO.input(self.button_pin)
+
+	def InitPhoto(self):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.photo_pin, GPIO.IN)
+		print("Pin {0} is currently: {1}".format(self.photo_pin,GPIO.input(self.photo_pin)))
+
+	def PhotoStatus(self):
+		return GPIO.input(self.photo_pin)
+
+
 
 class Ledarray:
 
